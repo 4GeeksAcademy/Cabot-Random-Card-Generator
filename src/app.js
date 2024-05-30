11efbb27e632ca89aae9ad85fd326e419df59cde
Jacob Cabot <166416094+JacobBCabot@users.noreply.github.com>
2024-05-30 00:46:24 +0000

    generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
+let cardNumber = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+let cardSuit = ["♣", "♦", "♥", "♠"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function randomNumber() {}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function randomCard() {
+  const randomNumber =
+    cardNumber[Math.floor(Math.random() * cardNumber.length)];
+  //console.log(randomNumber);
+  const randomSuit = cardSuit[Math.floor(Math.random() * cardSuit.length)];
+  //console.log(cardSuit[randomSuit]);
+  document.querySelector("#cardNumber").innerText = randomNumber;
+  document.querySelector("#cardSuit1").innerText = randomSuit;
+  document.querySelector("#cardSuit2").innerText = randomSuit;
+  if (randomSuit === cardSuit[1] || randomSuit === cardSuit[2]) {
+    document.querySelector("#cardSuit1").style.color = "red";
+    document.querySelector("#cardSuit2").style.color = "red";
+    document.querySelector("#cardNumber").style.color = "red";
+  } else {
+    document.querySelector("#cardSuit1").style.color = "black";
+    document.querySelector("#cardSuit2").style.color = "black";
+    document.querySelector("#cardNumber").style.color = "black";
+  }
 };
